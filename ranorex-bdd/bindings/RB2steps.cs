9b7a84fc1bf7f857a67872c38c43b3ddce382096
edgewords <info@edgewords.co.uk>
2018-03-31 11:42:05 +0100

    features done

---
+++ ranorex-bdd/bindings/RB2steps.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by Ranorex
+ * User: Tom
+ * Date: 31/03/2018
+ * Time: 11:39
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using TechTalk.SpecFlow;
+using Ranorex;
+using NUnit.Framework;
+
+namespace ranorex_bdd.bindings
+{
+    [Binding]
+    public class RB2steps
+    {
+
+        [When("I connect with Ranorex")]
+        public void WhenIPressAdd()
+        {
+            // TODO: implement act (action) logic
+
+            Ranorex.Report.Info("this is the first step");
+        }
+
+        [Then("it works")]
+        public void ThenTheResultShouldBe()
+        {
+        	Assert.That(true);
+        	
+        }
+    }
+}
--- ranorex-bdd/features/RB-2.feature
+++ ranorex-bdd/features/RB-2.feature
@@ -1,4 +1,4 @@
-
+@functional
 Feature: RB-2
   <Some interesting description here>
 
